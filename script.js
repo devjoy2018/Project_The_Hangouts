14d86239182ced3cc4491ecf115e7044772e1dac
Joydeep Dev <dev.joydeep2018@gmail.com>
2024-03-16 10:31:24 +0530

    button function

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+document.getElementById('bookNowBtn').addEventListener('click', function () {
+  document.getElementById('popup').style.display = 'block';
+  setTimeout(function () {
+    closePopup();
+  }, 5000);
+});
 
+// displaying the pop up block
+
+function closePopup() {
+  document.getElementById('popup').style.display = 'none';
+}
+
+document.querySelector('.close').addEventListener('click', function () {
+  document.getElementById('popup').style.display = 'none';
+});
+
+// closing pop up on click
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape') {
+    closePopup();
+  }
+});
+
+// closing popup on escape
